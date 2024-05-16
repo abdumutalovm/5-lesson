9c39b7e561e2d9cc91dddf9cd1595cf3ded98205
abdumutlovm <abdumutalovmirziyod83@gmail.com>
2024-05-17 02:51:31 +0500

    update Details and fixed errors

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -35,7 +35,17 @@ function Details() {
     }
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="w-3/5 p-10 rounded-xl mx-auto bg-zinc-400 text-white gap-4 mt-10 flex flex-col items-center">
+      <h1 className="text-4xl mb-10 text-zinc-500">About Phone</h1>
+      <h1 className="text-3xl font-bold">Name: {phone?.name}</h1>
+      <h2 className="text-3xl font-bold">Price: ${phone?.price}</h2>
+      <h2 className="text-3xl font-bold">Description: {phone?.description}</h2>
+      <h2 className="text-3xl font-bold">Status: {phone?.status}</h2>
+      <h2 className="text-3xl font-bold">Category id: {phone?.category_id}</h2>
+      <h3 className="text-3xl font-bold">Created at: {phone?.createdAt}</h3>
+    </div>
+  );
 }
 
 export default Details;
